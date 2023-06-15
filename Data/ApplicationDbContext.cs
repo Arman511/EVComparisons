0cbad984503472a34b3e965932cd9905ae36466c
Arman <arman.shaikh.work@gmail.com>
2023-06-15 19:13:17 +0100

    First COmmit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EVComparisons.Models;
+
+namespace EVComparisons.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EVComparisons.Models.Cars> Cars { get; set; } = default!;
+    }
+}
---
+++ Models/Cars.cs
@@ -0,0 +1,83 @@
+﻿namespace EVComparisons.Models
+{
+    public class Cars
+    {
+        public int Id { get; set; }
+        public ExtraInfo.CarTypes Type { get; set; }
+        public string Maker { get; set; }
+        public string Model { get; set; }
+        public int Range { get; set; }
+        public int FullPrice { get; set; }
+        public int Seats { get; set; }
+
+        public int Made { get; set; }
+        public int CargoVolume { get; set; }
+        public bool RoofRails { get; set; }
+        public bool TowHitch { get; set; }
+        public int TowWeight { get; set; }
+        public int MaxPayload { get; set; }
+        public int Safety { get; set; }
+        public double BatteryCapacity { get; set; }
+        public int NormalChargePower { get; set; }
+        public int NormalChargeTime { get; set; }
+        public ExtraInfo.PortTypes NormalChargePort { get; set; }
+        public ExtraInfo.Location NormalPortLocation { get; set; }
+        public int FastChargePower { get; set; }
+        public int FastChargeTime { get; set; }
+        public ExtraInfo.PortTypes FastChargePort { get; set; }
+        public ExtraInfo.Location FastPortLocation { get; set; }
+        public int TopSpeed { get; set; }
+        public double NaughtTo60 { get; set; }
+        public int Efficiency { get; set; }
+        public int TotalPower { get; set; }
+        public int TotalTorque { get; set; }
+        public ExtraInfo.CarDriveType Drive { get; set; }
+        public int Length { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+
+    public class ExtraInfo
+    {
+        public enum CarTypes
+        {
+            Hatchback,
+            Sedan,
+            SUV,
+            Coupe,
+            Convertible,
+            Minivan,
+            PickupTruck,
+            SportsCar,
+            Luxury,
+            Compact,
+            Crossover,
+            OffRoad,
+            StationWagon
+        }
+
+        public enum PortTypes
+        {
+            Type1,
+            Type2,
+            CHAdeMo,
+            CCS
+        }
+
+        public enum Location
+        {
+            FrontMiddle,
+            FrontLeft,
+            FrontRight,
+            RearLeft,
+            RearRight
+        }
+
+        public enum CarDriveType
+        {
+            Rear,
+            Front,
+            AllWheel
+        }
+    }
+}
