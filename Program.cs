33d0e08d7fac0e7c3d1a371f057e63341bb51a2d
Arman <arman.shaikh.work@gmail.com>
2023-06-18 21:29:49 +0100

    Finally got the website to work

--- Program.cs
+++ Program.cs
@@ -28,6 +28,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Cars}/{action=Index}/");
 
 app.Run();
